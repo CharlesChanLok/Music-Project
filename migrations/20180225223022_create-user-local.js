febe61d33199ca64855d0da78effdca5975eccfc
CharlesChanLok <charleschanlok@gmail.com>
2018-02-25 22:40:50 +0800

    added local-auth migration file and local-auth-router

--- migrations/20180225223022_create-user-local.js
+++ migrations/20180225223022_create-user-local.js
@@ -1,15 +1,15 @@
 
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable("users_google", (table) => {
+    return knex.schema.createTable("users_local", (table) => {
         table.increments();
         table.string("firstname");
         table.string("lastname");
-        table.string("gmail");
-        table.string("googleid");
+        table.string('account')
+        table.string("password");
         table.timestamps(false, true);
     });
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTable("users_google");
+    return knex.schema.dropTable("users_local");
 };
